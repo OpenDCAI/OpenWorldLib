03dba03ad032a291ef18c793e7e847b0bc3d1315
Chriszkxxx <kaixinzhu56@gmail.com>
2025-11-29 03:36:09 +0800

    Optimize the integration of DepthAnything_2

--- test/test_depth_anything.py
+++ test/test_depth_anything.py
@@ -8,8 +8,8 @@ from src.sceneflow.pipelines.depth_anything.pipeline_depth_anything import (
 
 # Configure before running
 DATA_TYPE = "image"  # or "video"
-DATA_PATH = "/YOUR/IMAGE/OR/VIDEO/PATH"
-MODEL_PATH = "/YOUR/MODEL/PATH"
+DATA_PATH = "/YOUR/IMAGE/OR/VIDEO/PATH" 
+MODEL_PATH = "LiheYoung/depth_anything_vitl14"
 
 ENCODER = "vitl"
 OUTPUT_DIR = str(Path(__file__).parent / ("vis_depth" if DATA_TYPE == "image" else "vis_video_depth"))
