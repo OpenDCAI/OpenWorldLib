55ac2585653bc7676573c6acef6a235d04a35deb
yfanDai <moonwines21@gmail.com>
2025-12-05 15:30:27 +0800

    Add BaseReasoning

---
+++ src/sceneflow/reasoning/base_reasoning.py
@@ -0,0 +1,15 @@
+
+class BaseReasoning(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_path, device=None, **kwargs):
+        pass
+    
+    def api_init(self, api_key, endpoint):
+        pass
+
+    @torch.no_grad()
+    def inference(self):
+        pass
