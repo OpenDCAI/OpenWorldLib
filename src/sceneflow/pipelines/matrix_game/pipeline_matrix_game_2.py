d8d73aa7c4bcc8f0bdbc9314532d35dee7e3dc35
zengbohan0217 <13811753844@126.com>
2026-01-07 19:34:08 +0800

    refine matrix-game 2 operator

--- src/sceneflow/pipelines/matrix_game/pipeline_matrix_game_2.py
+++ src/sceneflow/pipelines/matrix_game/pipeline_matrix_game_2.py
@@ -20,12 +20,6 @@ class MatrixGame2Pipeline:
         self.device = device
         self.weight_dtype = weight_dtype
 
-        self.frame_process = v2.Compose([
-            v2.Resize(size=(352, 640), antialias=True),
-            v2.ToTensor(),
-            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        ])
-
     @classmethod
     def from_pretrained(cls,
                         synthesis_model_path: Optional[str] = None,
@@ -53,21 +47,6 @@ class MatrixGame2Pipeline:
             weight_dtype=weight_dtype
         )
         return pipeline
-
-    def _resizecrop(self, image, th, tw):
-        w, h = image.size
-        if h / w > th / tw:
-            new_w = int(w)
-            new_h = int(new_w * th / tw)
-        else:
-            new_h = int(h)
-            new_w = int(new_h * tw / th)
-        left = (w - new_w) / 2
-        top = (h - new_h) / 2
-        right = (w + new_w) / 2
-        bottom = (h + new_h) / 2
-        image = image.crop((left, top, right, bottom))
-        return image
     
     def process(self,
                 input_image,
@@ -80,17 +59,15 @@ class MatrixGame2Pipeline:
         """
         the input_image is PIL image
         """
-        image = self._resizecrop(input_image, resize_H, resize_W)
-        image = self.frame_process(image)[None, :, None, :, :].to(dtype=self.weight_dtype, device=self.device)
+        preception_dict = self.operators.process_perception(input_image, num_output_frames, resize_H, resize_W,
+                                                            device=self.device, weight_dtype=self.weight_dtype)
 
-        padding_video = torch.zeros_like(image).repeat(1, 1, 4 * (num_output_frames - 1), 1, 1)
-        img_cond = torch.concat([image, padding_video], dim=2)
-        tiler_kwargs={"tiled": True, "tile_size": [resize_H//8, resize_W//8], "tile_stride": [resize_H//16+1, resize_W//16-2]}
-        img_cond = self.synthesis_model.vae.encode(img_cond, device=self.device, **tiler_kwargs).to(self.device)
+        img_cond = self.synthesis_model.vae.encode(preception_dict["img_cond"], device=self.device,
+                                                   **preception_dict["tiler_kwargs"]).to(self.device)
         mask_cond = torch.ones_like(img_cond)
         mask_cond[:, :, 1:] = 0
         cond_concat = torch.cat([mask_cond[:, :4], img_cond], dim=1) 
-        visual_context = self.synthesis_model.vae.clip.encode_video(image)
+        visual_context = self.synthesis_model.vae.clip.encode_video(preception_dict["image"])
 
         output_dict = {
             "cond_concat": cond_concat,
