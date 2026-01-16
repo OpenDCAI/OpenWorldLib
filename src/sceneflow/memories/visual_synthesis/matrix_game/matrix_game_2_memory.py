4b2a62e523cf557e9e64b7c1486b5d9905299398
zengbohan0217 <13811753844@126.com>
2026-01-16 14:29:54 +0800

    finish basic matrix game stream

--- src/sceneflow/memories/visual_synthesis/matrix_game/matrix_game_2_memory.py
+++ src/sceneflow/memories/visual_synthesis/matrix_game/matrix_game_2_memory.py
@@ -12,31 +12,37 @@ def tensor_to_pil(tensor: np.ndarray) -> Image.Image:
 
 class MatrixGame2Memory(BaseMemory):
     """
-    MatrixGame2 专用的 Memory 模块
-    storage 为列表，current image 取最后一个
+    MatrixGame2 Memory module
     """
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.storage = []
+        self.all_frames = []
 
     def record(self, data, **kwargs):
-        """记录帧到 storage"""
+        """record to storage"""
         if isinstance(data, Image.Image):
-            self.storage.append(data)
+            current_image = data
         elif isinstance(data, list):
             last_frame = data[-1]
-            self.storage.append(tensor_to_pil(last_frame))
+            current_image = tensor_to_pil(last_frame)
+            self.all_frames.extend(data)
+        self.storage.append({
+            'content': current_image,
+            'type': 'image',
+            'timestamp': len(self.all_frames),
+            'metadata': {}
+        })
 
     def select(self, **kwargs) -> Optional[Image.Image]:
-        """获取最后一帧作为 current image"""
+        """select the last frame as current image"""
         if len(self.storage) == 0:
             return None
-        return self.storage[-1]
+        return self.storage[-1]['content']
 
     def manage(self, action: str = "reset", **kwargs):
-        """管理 storage"""
+        """manage storage"""
         if action == "reset":
             self.storage = []
-
-## matrix game 2 memory可以利用first image改成非常简单的记忆处理机制，可以处理折返运动有问题的时候
+            self.all_frames = []
