5b2bc55e94e7d876522cfc706348809a16152e7c
Nijikadesu <mingkun502@gmail.com>
2026-02-10 18:49:47 +0800

    feat(operator): resize cosmos input image

--- src/sceneflow/operators/cosmos_predict2p5_operator.py
+++ src/sceneflow/operators/cosmos_predict2p5_operator.py
@@ -1,6 +1,7 @@
 import logging
 from PIL import Image
 from pathlib import Path
+from giga_datasets import image_utils
 from typing import Any, Dict, Optional, Union
 
 from .base_operator import BaseOperator
@@ -44,9 +45,6 @@ class CosmosPrecict2p5Operator(BaseOperator):
 
     def process_interaction(
         self,
-        task: str,
-        image: Optional[Image.Image] = None,
-        base_seed: int = -1,
         **kwargs,
     ) -> Dict[str, Any]:
         if len(self.current_interaction) == 0:
@@ -54,19 +52,38 @@ class CosmosPrecict2p5Operator(BaseOperator):
         prompt = self.current_interaction[-1]
         self.interaction_history.append(prompt)
         return {
-            "processed_prompt": prompt,
+            "input_prompt": prompt,
         }
 
     def process_perception(
         self,
         input_path: Optional[Union[str, Path, Image.Image]] = None,
+        height: int = 704,
+        width: int = 1280,
         **kwargs,
     ) -> Dict[str, Any]:
         """Path to the conditioning image (for `img2world` task), Optional."""
-        if input_path is None:
-            input_image = None
-        else:
+        if input_path is not None:
             input_image = _load_input_image(input_path)
+            # Resize & crop to fit the input size
+            image_width, image_height = input_image.width, input_image.height
+            dst_width, dst_height = image_utils.get_image_size((image_width, image_height), (width, height), mode='area', multiple=16)
+            if float(dst_height) / image_height < float(dst_width) / image_width:
+                new_height = int(round(float(dst_width) / image_width * image_height))
+                new_width = dst_width
+            else:
+                new_height = dst_height
+                new_width = int(round(float(dst_height) / image_height * image_width))
+            assert dst_width <= new_width and dst_height <= new_height
+            x1 = (new_width - dst_width) // 2
+            y1 = (new_height - dst_height) // 2
+            input_image = F.resize(image, (new_height, new_width), InterpolationMode.BILINEAR)
+            input_image = F.crop(input_image, y1, x1, dst_height, dst_width)
+        else:
+            input_image = None
+            dst_width, dst_height = width, height
         return {
             "input_image": input_image,
+            "height": image_height,
+            "width": image_width,
         }
